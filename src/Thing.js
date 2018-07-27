0d5f3c758d33c6c75b920fa038eabce904417f7e
Jared Palmer <jared@palmer.net>
2018-07-27 16:57:18 -0400

    Add example use of Placeholder

---
+++ src/Thing.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Thing() {
+  return <div>Thing!!!!!</div>;
+}
+
+export default Thing;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import { unstable_createRoot as createRoot } from 'react-dom';
+import { createCache, createResource } from 'simple-cache-provider';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const cache = createCache();
+
+// Lazy load!
+const getThing = createResource(
+  () => sleep(1000).then(() => import('./Thing').then(mod => mod.default)),
+  thing => thing
+);
+
+const Thing = props => {
+  const Comp = getThing.read(cache, props);
+  return <Comp {...props} />;
+};
 
 function App() {
-  return <span className="big">hello</span>;
+  return (
+    <React.Fragment>
+      <h1>Suspense</h1>
+      <React.Placeholder delayMs={500} fallback={<div>🌀 'Loading....'</div>}>
+        <Thing />
+      </React.Placeholder>
+    </React.Fragment>
+  );
 }
 
 createRoot(window.app).render(<App />);
+
+if (module.hot) {
+  module.hot.accept();
+}
